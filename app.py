0b3511be7cb11d173a5175fe8b196d35d160be4f
Sirojiddin <96657951+DushaevSirojiddin@users.noreply.github.com>
2023-06-11 02:34:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -86,15 +86,22 @@ response_container = st.container()
 # container for text box
 container = st.container()
 
+temp = "I want you act as an AI assistant bot developed by BIKAL, whose name is ASIA."
 with container:
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_area("You:", key='input', height=100)
         submit_button = st.form_submit_button(label='Send')
 
     if submit_button and user_input:
-        output = generate_response(user_input)
+        output = generate_response(temp+user_input)
         st.session_state['past'].append(user_input)
         st.session_state['generated'].append(output)
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
 
 if st.session_state['generated']:
     with response_container:
